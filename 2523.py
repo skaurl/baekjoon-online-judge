315009e401e90b2579f6e13ac374490de4891697
kimnamhyeok <dr_lunars@naver.com>
2020-10-13 10:55:05 +0900

    practice

---
+++ 2523.py
@@ -0,0 +1,9 @@
+import sys
+N = int(sys.stdin.readline())
+A = []
+for i in range(N):
+    A.append('*'*(i+1))
+for i in range(len(A)):
+    print(A[i])
+for i in range(len(A)-1):
+    print(A[N-i-2])
