e78a37d61cea20b7cce309ff3ab1beb1755f318a
kimnamhyeok <dr_lunars@naver.com>
2020-10-25 22:17:47 +0900

    practice

---
+++ 11051.py
@@ -0,0 +1,11 @@
+import sys
+
+def f(x):
+    result = 1
+    for i in range(x):
+        result *= i+1
+    return result
+
+n = sys.stdin.readline().split()
+
+print(int(f(int(n[0])) // (f(int(n[1]))*f(int(n[0])-int(n[1])))) % 10007)
