926ac21790a8a534efba68483d0882f3d26f566e
kimnamhyeok <dr_lunars@naver.com>
2020-12-26 20:22:13 +0900

    practice

--- 2609.py
+++ 2609.py
@@ -1,37 +1,12 @@
-import sys
-n = list(map(int,sys.stdin.readline().split()))
-m_1 = n[0]
-m_2 = n[1]
-n_1 = n[0]
-n_2 = n[1]
-N_1 = []
-N_2 = []
+def gcd(x,y):
+    while y:
+        x, y = y, x%y
+    return x
 
-i = 2
-while n_1 != 1:
-    if n_1%i==0:
-        N_1.append(i)
-        n_1 /= i
-    else:
-        i += 1
+def lcm(x,y):
+    return x*y//gcd(x,y)
 
-i = 2
-while n_2 != 1:
-    if n_2%i==0:
-        N_2.append(i)
-        n_2 /= i
-    else:
-        i += 1
+a,b = map(int,input().split())
 
-A = list(set(N_1)&set(N_2))
-
-lcm = 1
-
-for i in range(len(A)):
-    if N_1.count(A[i]) >= N_2.count(A[i]):
-        lcm *= N_2.count(A[i]) * A[i]
-    else:
-        lcm *= N_1.count(A[i]) * A[i]
-
-print(lcm)
-print(lcm * (m_1//lcm) * (m_2//lcm))
+print(gcd(a,b))
+print(lcm(a,b))
